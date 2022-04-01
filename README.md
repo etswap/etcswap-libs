551b330c9828a33b581f6db579c508ad07ed99fc
NHdj <NHdj@protonmail.com>
2022-04-01 15:42:49 +0800

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Keyneswap SDK
+
+
---
+++ src/constants.ts
@@ -0,0 +1,48 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 61,
+  OKT = 65,
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT,
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP,
+}
+
+export const FACTORY_ADDRESS = '0x8Cb6d4c2aF3446F402E0e41d9A4966Aaf1539e8c'
+
+export const INIT_CODE_HASH = '0x1740331b07cf3fb2b3cdb09fe0e6f504cd06471051037a57315561a089d09741'
+                              
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _998 = JSBI.BigInt(998)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256',
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'),
+}
